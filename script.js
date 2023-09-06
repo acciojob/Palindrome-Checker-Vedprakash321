0a0ad46bb85c12af538cad2d2d82ef0f207ec3de
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-06 20:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	  return cleanedStr === cleanedStr.split('').reverse().join('');
+	// let i=0;
+	// let j=str.length-1;
+	// while(i<j){
+	// 	if(str[i]!=str[j]){
+	// 		return false;
+	// 	}
+	// 	i++;
+	// 	j--;
+		
+	// }
+ //    return true;
 }
 module.exports = palindrome
